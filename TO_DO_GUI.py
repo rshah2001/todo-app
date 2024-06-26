c3cde0a68678d318c3678e6609a9a69fed668298
Rishil <rishil1211@outlook.com>
2024-06-26 17:53:41 -0400

    Implemented the Add button

--- TO_DO_GUI.py
+++ TO_DO_GUI.py
@@ -1,19 +1,37 @@
 import functions
-import FreeSimpleGUI as sg
+import FreeSimpleGUI as Sg
 
 # this creates a label on the window
-label = sg.Text("Type in a to-do")
+label = Sg.Text("Type in a to-do")
 
 # creating an input box
-input_box = sg.InputText(tooltip= "Enter a Todo")
+input_box = Sg.InputText(tooltip="Enter a Todo", key="todo")
 
 # adding a button
-add_button = sg.Button("Add")
+add_button = Sg.Button("Add")
 
 # This will be the title of the window.
 # this layout expects a list. Items placed inside the inner brackets will be placed in one row
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window = Sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
 '''put the input box in next line; put both of them in separate [] '''
 
-window.read()  # this displays the window on the screen
+# using a while loop so it keeps the window open
+while True:
+    # Event will read the button and value will display the text written in it
+    event, values = window.read()  # this displays the window on the screen
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            # the values below will print a dictionary
+            new_todo = values['todo'] + "\n"
+            todos.append(new_todo)  # this will have the new todo from the new_todo and the add button
+            functions.write_todos(todos)
+        case Sg.WIN_CLOSED:
+            break
+
+
 window.close()
